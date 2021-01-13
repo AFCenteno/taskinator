08c6efcaab93832f39eb7d765cbebaf05f6f656e
mrafcent1 <alexfcenteno@outlook.com>
2021-01-13 12:43:20 -0500

    Tasks added with inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var formEl = document.querySelector("#task-form")
+var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function(event){
     event.preventDefault();
+    var taskNameInput= document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value
 
     var taskitemEl = document.createElement("li");
-    taskitemEl.textContent = "This is a new task.";
     taskitemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    taskitemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(taskitemEl);
 }    
 
